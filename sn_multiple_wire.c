931897358bb78d0d489091d08a01a795f02fcfb9
wolf <lukablurr@yahoo.com>
2012-04-17 23:55:45 +0300

    completed

--- sn_multiple_wire.c
+++ sn_multiple_wire.c
@@ -5,22 +5,99 @@
  *      Author: Costin Lupu
  */
 
+#include "n2n.h"
 #include "sn_multiple_wire.h"
 
 
-int encode_snm_hdr( uint8_t *base,
+
+static int alloc_array( void **array_ptr, unsigned int num, unsigned int item_size )
+{
+    if (num > 0)
+    {
+        *array_ptr = calloc(num, item_size);
+        if (!*array_ptr)
+        {
+            traceEvent(TRACE_ERROR, "not enough memory for allocating array");
+            return -1;
+        }
+    }
+    else
+    {
+        *array_ptr = NULL;
+    }
+
+    return 0;
+}
+static void free_array( void **array_ptr )
+{
+    if(array_ptr && *array_ptr)
+    {
+        free(*array_ptr);
+        *array_ptr = NULL;
+    }
+}
+
+int alloc_communities( snm_comm_name_t **comm_ptr, unsigned int comm_num )
+{
+    return alloc_array((void **) comm_ptr, comm_num, sizeof(snm_comm_name_t));
+}
+void free_communities( snm_comm_name_t **comm_ptr )
+{
+    free_array((void **) comm_ptr);
+}
+
+int alloc_supernodes( n2n_sock_t **sn_ptr, unsigned int sn_num )
+{
+    return alloc_array((void **) sn_ptr, sn_num, sizeof(n2n_sock_t));
+}
+void free_supernodes( n2n_sock_t **sn_ptr )
+{
+    free_array((void **) sn_ptr);
+}
+
+int encode_SNM_comm( uint8_t *base,
+                     size_t  *idx,
+                     const snm_comm_name_t *comm_name )
+{
+    int retval = 0;
+    retval += encode_uint8(base, idx, comm_name->size);
+    retval += encode_buf(base, idx, comm_name->name, comm_name->size);
+    return retval;
+}
+
+int decode_SNM_comm( snm_comm_name_t *comm_name,
+                     const uint8_t *base,
+                     size_t *rem,
+                     size_t *idx )
+{
+    int retval = 0;
+    memset(comm_name, 0, N2N_COMMUNITY_SIZE);
+    retval += decode_uint8(&comm_name->size, base, rem, idx);
+    retval += decode_buf(comm_name->name, comm_name->size, base, rem, idx);
+    return retval;
+}
+
+int encode_SNM_hdr( uint8_t *base,
                     size_t  *idx,
                     const snm_hdr_t *hdr )
 {
-    return 0;
+    int retval = 0;
+    retval += encode_uint8(base, idx, hdr->type);
+    retval += encode_uint8(base, idx, hdr->flags);
+    retval += encode_uint16(base, idx, hdr->seq_num);
+    return retval;
 }
 
-int decode_snm_hdr( snm_hdr_t     *hdr,
+int decode_SNM_hdr( snm_hdr_t     *hdr,
                     const uint8_t *base,
                     size_t *rem,
                     size_t *idx )
 {
-    return 0;
+    int retval = 0;
+    retval += decode_uint8(&hdr->type, base, rem, idx);
+    retval += decode_uint8(&hdr->flags, base, rem, idx);
+    retval += decode_uint16(&hdr->seq_num, base, rem, idx);
+    return retval;
 }
 
 int encode_SNM_REQ( uint8_t *base,
@@ -28,34 +105,103 @@ int encode_SNM_REQ( uint8_t *base,
                     const snm_hdr_t     *hdr,
                     const n2n_SNM_REQ_t *req )
 {
-    return 0;
+    int retval = 0;
+    retval += encode_SNM_hdr(base, idx, hdr);
+    if (GET_N(hdr->flags))
+    {
+        retval += encode_uint16(base, idx, req->comm_num);
+
+        int i;
+        for (i = 0; i < req->comm_num; i++)
+        {
+            retval += encode_SNM_comm(base, idx, &req->comm_ptr[i]);
+        }
+    }
+    return retval;
 }
 
 int decode_SNM_REQ( n2n_SNM_REQ_t    *pkt,
                     const snm_hdr_t  *hdr,
                     const uint8_t    *base,
                     size_t *rem,
-                     size_t *idx )
+                    size_t *idx )
 {
-    return 0;
+    int retval = 0;
+
+    if (GET_N(hdr->flags))
+    {
+        retval += decode_uint16(&pkt->comm_num, base, rem, idx);
+
+        if (alloc_communities(&pkt->comm_ptr, pkt->comm_num))
+        {
+            return -1;
+        }
+
+        int i;
+        for (i = 0; i < pkt->comm_num; i++)
+        {
+            retval += decode_SNM_comm(&pkt->comm_ptr[i], base, rem, idx);
+        }
+    }
+    return retval;
 }
 
-int encode_SNM_RSP( uint8_t *base,
-                    size_t  *idx,
-                    const snm_hdr_t     *hdr,
-                    const n2n_SNM_RSP_t *rsp )
+int encode_SNM_INFO( uint8_t *base,
+                     size_t  *idx,
+                     const snm_hdr_t      *hdr,
+                     const n2n_SNM_INFO_t *info )
 {
-    return 0;
+    int retval = 0;
+    retval += encode_SNM_hdr(base, idx, hdr);
+    retval += encode_uint16(base, idx, info->sn_num);
+    retval += encode_uint16(base, idx, info->comm_num);
+
+    int i;
+    for (i = 0; i < info->sn_num; i++)
+    {
+        retval += encode_sock(base, idx, &info->sn_ptr[i]);
+    }
+
+    for(i = 0; i < info->comm_num; i++)
+    {
+        retval += encode_SNM_comm(base, idx, &info->comm_ptr[i]);
+    }
+    return retval;
 }
 
-int decode_SNM_RSP( n2n_SNM_RSP_t   *pkt,
-                    const snm_hdr_t *hdr,
-                    const uint8_t   *base,
-                    size_t * rem,
-                    size_t * idx )
+int decode_SNM_INFO( n2n_SNM_INFO_t   *pkt,
+                     const snm_hdr_t  *hdr,
+                     const uint8_t    *base,
+                     size_t * rem,
+                     size_t * idx )
 {
-    return 0;
+    int retval = 0;
+    retval += decode_uint16(&pkt->sn_num, base, rem, idx);
+    retval += decode_uint16(&pkt->comm_num, base, rem, idx);
+
+    if (alloc_supernodes(&pkt->sn_ptr, pkt->sn_num))
+    {
+        return -1;
+    }
+    if (alloc_communities(&pkt->comm_ptr, pkt->comm_num))
+    {
+        free_supernodes(&pkt->sn_ptr);
+        return -1;
+    }
+
+    int i;
+    for (i = 0; i < pkt->sn_num; i++)
+    {
+        retval += decode_sock(&pkt->sn_ptr[i], base, rem, idx);
+    }
+    for (i = 0; i < pkt->comm_num; i++)
+    {
+        retval += decode_SNM_comm(&pkt->comm_ptr[i], base, rem, idx);
+    }
+
+    return retval;
 }
+/*
 
 int encode_ADVERTISE_ME( uint8_t *base,
                          size_t  *idx,
@@ -72,4 +218,39 @@ int decode_ADVERTISE_ME( n2n_SNM_ADV_ME_t  *pkt,
                          size_t *idx )
 {
     return 0;
+}
+*/
+void log_SNM_hdr( const snm_hdr_t *hdr )
+{
+    traceEvent( TRACE_DEBUG, "HEADER type=%d S=%d C=%d N=%d A=%d Seq=%d", hdr->type,
+                GET_S(hdr->flags), GET_C(hdr->flags), GET_N(hdr->flags), GET_A(hdr->flags),
+                hdr->seq_num );
+}
+void log_SNM_REQ( const n2n_SNM_REQ_t *req )
+{
+    int i;
+    traceEvent( TRACE_DEBUG, "REQ Communities=%d", req->comm_num );
+    for(i = 0; i < req->comm_num; i++)
+    {
+        traceEvent( TRACE_DEBUG, "\t[%d] len=%d name=%s", i,
+                    req->comm_ptr[i].size, req->comm_ptr[i].name );
+    }
+}
+void log_SNM_INFO( const n2n_SNM_INFO_t *info )
+{
+    int i;
+    n2n_sock_str_t sockbuf;;
+
+    traceEvent( TRACE_DEBUG, "INFO Supernodes=%d Communities=%d",
+                info->sn_num, info->comm_num );
+
+    for(i = 0; i < info->sn_num; i++)
+    {
+        traceEvent( TRACE_DEBUG, "\t[S%d] %s", i, sock_to_cstr(sockbuf, &info->sn_ptr[i]) );
+    }
+    for(i = 0; i < info->comm_num; i++)
+    {
+        traceEvent( TRACE_DEBUG, "\t[C%d] len=%d name=%s", i, info->comm_ptr[i].size,
+                    (info->comm_ptr[i].size > 0) ? (const char *) info->comm_ptr[i].name : "" );
+    }
 }
--- sn_multiple_wire.h
+++ sn_multiple_wire.h
@@ -26,50 +26,94 @@ typedef struct snm_hdr
 #define IS_RSP_LIST(pMsg)        ((pMsg)->hdr.type == SNM_TYPE_RSP_LIST_MSG)
 #define IS_ADV_ME(pMsg)          ((pMsg)->hdr.type == SNM_TYPE_ADV_ME_MSG)
 
+/*
+ * Flags (MSB order):
+ *  0 1 2 3 4 5 6 7
+ * +-+-+-+-+-+-+-+-+
+ * |S|C|N|A| rfu   |
+ * +-+-+-+-+-+-+-+-+
+ */
+#define S_FLAG_POS       7
+#define C_FLAG_POS       6
+#define N_FLAG_POS       5
+#define A_FLAG_POS       4
+
+#define __FLAG_MASK(pos)           (1 << (pos))
+#define __BIT_FLAG(flags, pos)     (((flags) >> (pos)) & 1)
+
+#define SET_S(flags)      (flags) |= __FLAG_MASK(S_FLAG_POS)
+#define SET_C(flags)      (flags) |= __FLAG_MASK(C_FLAG_POS)
+#define SET_N(flags)      (flags) |= __FLAG_MASK(N_FLAG_POS)
+#define SET_A(flags)      (flags) |= __FLAG_MASK(A_FLAG_POS)
+
+#define GET_S(flags)      __BIT_FLAG(flags, S_FLAG_POS)
+#define GET_C(flags)      __BIT_FLAG(flags, C_FLAG_POS)
+#define GET_N(flags)      __BIT_FLAG(flags, N_FLAG_POS)
+#define GET_A(flags)      __BIT_FLAG(flags, A_FLAG_POS)
+
+#define CLR_S(flags)      (flags) &= ~__FLAG_MASK(S_FLAG_POS)
+#define CLR_C(flags)      (flags) &= ~__FLAG_MASK(C_FLAG_POS)
+#define CLR_N(flags)      (flags) &= ~__FLAG_MASK(N_FLAG_POS)
+#define CLR_A(flags)      (flags) &= ~__FLAG_MASK(A_FLAG_POS)
+
 typedef struct snm_comm_name
 {
     uint8_t           size;
-    uint8_t          *name;
+    /*uint8_t          *name;*/
+    n2n_community_t   name;
 } snm_comm_name_t;
 
-typedef struct n2n_REQ_LIST
+typedef struct n2n_SNM_REQ_INFO
 {
     /*snm_hdr_t         hdr;*/
     uint16_t          comm_num;
     snm_comm_name_t  *comm_ptr;
 } n2n_SNM_REQ_t;
 
-typedef struct n2n_RSP_LIST
+typedef struct n2n_SNM_INFO
 {
     /*snm_hdr_t         hdr;*/
     uint16_t          sn_num;
     uint16_t          comm_num;
     n2n_sock_t       *sn_ptr;
     snm_comm_name_t  *comm_ptr;
-} n2n_SNM_RSP_t;
+} n2n_SNM_INFO_t;
+/*
 
 typedef struct n2n_ADVERTISE_ME
 {
-    /*snm_hdr_t         hdr;*/
     n2n_sock_t        sn;
     uint16_t          comm_num;
     snm_comm_name_t  *comm_ptr;
 } n2n_SNM_ADV_ME_t;
+*/
 
+int alloc_communities( snm_comm_name_t **comm_ptr, unsigned int comm_num );
+void free_communities( snm_comm_name_t **comm_ptr );
+int alloc_supernodes( n2n_sock_t **sn_ptr, unsigned int sn_num );
+void free_supernodes( n2n_sock_t **sn_ptr );
 
-int encode_snm_hdr( uint8_t *base,
+int encode_SNM_comm( uint8_t *base,
+                     size_t  *idx,
+                     const snm_comm_name_t *comm_name );
+int decode_SNM_comm( snm_comm_name_t *comm_name,
+                     const uint8_t *base,
+                     size_t *rem,
+                     size_t *idx );
+
+int encode_SNM_hdr( uint8_t *base,
                     size_t  *idx,
                     const snm_hdr_t *hdr );
 
-int decode_snm_hdr( snm_hdr_t     *hdr,
+int decode_SNM_hdr( snm_hdr_t     *hdr,
                     const uint8_t *base,
                     size_t *rem,
                     size_t *idx );
 
 int encode_SNM_REQ( uint8_t *base,
-                     size_t  *idx,
-                     const snm_hdr_t      *hdr,
-                     const n2n_SNM_REQ_t  *req );
+                    size_t  *idx,
+                    const snm_hdr_t      *hdr,
+                    const n2n_SNM_REQ_t  *req );
 
 int decode_SNM_REQ( n2n_SNM_REQ_t    *pkt,
                     const snm_hdr_t  *hdr,
@@ -77,16 +121,17 @@ int decode_SNM_REQ( n2n_SNM_REQ_t    *pkt,
                     size_t *rem,
                     size_t *idx );
 
-int encode_SNM_RSP( uint8_t *base,
-                    size_t  *idx,
-                    const snm_hdr_t      *hdr,
-                    const n2n_SNM_RSP_t  *rsp );
+int encode_SNM_INFO( uint8_t *base,
+                     size_t  *idx,
+                     const snm_hdr_t      *hdr,
+                     const n2n_SNM_INFO_t *rsp );
 
-int decode_SNM_RSP( n2n_SNM_RSP_t   *pkt,
-                    const snm_hdr_t *hdr,
-                    const uint8_t   *base,
-                    size_t * rem,
-                    size_t * idx );
+int decode_SNM_INFO( n2n_SNM_INFO_t   *pkt,
+                     const snm_hdr_t  *hdr,
+                     const uint8_t    *base,
+                     size_t * rem,
+                     size_t * idx );
+/*
 
 int encode_ADVERTISE_ME( uint8_t *base,
                          size_t  *idx,
@@ -98,5 +143,11 @@ int decode_ADVERTISE_ME( n2n_SNM_ADV_ME_t *pkt,
                          const uint8_t    *base,
                          size_t *rem,
                          size_t *idx );
+*/
+
+void log_SNM_hdr( const snm_hdr_t *hdr );
+void log_SNM_REQ( const n2n_SNM_REQ_t *req );
+void log_SNM_INFO( const n2n_SNM_INFO_t *info );
+
 
 #endif /* SN_MULTIPLE_WIRE_H_ */
