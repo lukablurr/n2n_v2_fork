4e5d29b1fbcd14964bdcc0648f573c5e2723b093
wolf <lukablurr@yahoo.com>
2012-04-17 23:56:07 +0300

    completed unit testing for lists and supernodes messages

--- sn_multiple_test.c
+++ sn_multiple_test.c
@@ -25,19 +25,138 @@ typedef int (*dec_func) ( void            *msg,
                           size_t          *rem,
                           size_t          *idx );
 
+typedef int (*cmp_func) ( const void *a, const void *b );
+
+typedef void (*add_func) (void **list, void *new);
+typedef void (*rand_func) (void *item);
+
+
+/*
+ * RANDOM functions
+ */
+static void rand_sn(struct sn_info *si)
+{
+    si->next = NULL;
+    si->sn.family = AF_INET;
+    unsigned int ipv4 = random() % 0xFFFFFFFF;
+    memcpy(si->sn.addr.v4, &ipv4, IPV4_SIZE);
+    si->sn.port = random() % ((1 << 16) - 1);
+}
+
+static void rand_comm(struct comm_info *ci)
+{
+    ci->next = NULL;
+    ci->sn_num = random() * 10;
+
+    int i = 0, name_size = random() % (N2N_COMMUNITY_SIZE - 1) + 1;
+    for (; i < name_size; i++)
+    {
+        ci->community_name[i] = random() % ('z' - 'a') + 'a';
+    }
+    ci->community_name[i] = 0;
+}
+
+static size_t generate_random_list(void          **list,
+                                   size_t          item_size,
+                                   rand_func       rand_item,
+                                   add_func        add)
+{
+    int i = 0, size = random() % 100;
+    traceEvent( TRACE_INFO, "Generating %d list items\n", size);
+
+    for (i = 0; i < size; i++)
+    {
+        void *new = calloc(1, item_size);
+        rand_item(new);
+        add(list, new);
+    }
+
+    return size;
+}
+
+/*
+ * COMPARE functions
+ */
+static int cmp_SNM_REQ( const void *a, const void *b )
+{
+    const n2n_SNM_REQ_t *reqA = (const n2n_SNM_REQ_t *) a;
+    const n2n_SNM_REQ_t *reqB = (const n2n_SNM_REQ_t *) b;
+
+    int i, diff = reqA->comm_num - reqB->comm_num;
+    if (diff)
+    {
+        return diff;
+    }
+    for(i = 0; i < reqA->comm_num; i++)
+    {
+        diff = memcmp(reqA->comm_ptr[i].name,
+                      reqB->comm_ptr[i].name,
+                      reqA->comm_ptr[i].size);
+        if (diff)
+        {
+            return diff;
+        }
+    }
+
+    return 0;
+}
+
+static int cmp_SNM_INFO( const void *a, const void *b )
+{
+    const n2n_SNM_INFO_t *infoA = (const n2n_SNM_INFO_t *) a;
+    const n2n_SNM_INFO_t *infoB = (const n2n_SNM_INFO_t *) b;
+
+    int i, diff = infoA->sn_num - infoB->sn_num;
+    if (diff)
+    {
+        return diff;
+    }
+    diff = infoA->comm_num - infoB->comm_num;
+    if (diff)
+    {
+        return diff;
+    }
+
+    for(i = 0; i < infoA->sn_num; i++)
+    {
+        diff = sock_equal(infoA->sn_ptr + i, infoB->sn_ptr + i);
+        if (diff)
+        {
+            return diff;
+        }
+    }
+
+    for(i = 0; i < infoA->comm_num; i++)
+    {
+        diff = memcmp(infoA->comm_ptr[i].name,
+                      infoB->comm_ptr[i].name,
+                      infoA->comm_ptr[i].size);
+        if (diff)
+        {
+            return diff;
+        }
+    }
+
+    return 0;
+}
+
+/*
+ * TESTING functions for MESSAGES
+ */
 static int test_SNM_MSG(size_t           struct_size,
                         const snm_hdr_t *hdr,
                         const void      *msg,
                         size_t           size,
                         enc_func         enc,
-                        dec_func         dec)
+                        dec_func         dec,
+                        cmp_func         cmp)
 {
-    uint8_t buf[4096];
+    uint8_t buf[1024 * 1024];
     snm_hdr_t new_hdr;
     void *new_msg = NULL;
     size_t idx = 0, rem = size;
 
-    if (enc(buf, &idx, hdr, msg))
+    if (enc(buf, &idx, hdr, msg) != idx)
     {
         traceEvent(TRACE_ERROR, "Error encoding message");
         return -1;
@@ -45,21 +164,40 @@ static int test_SNM_MSG(size_t           struct_size,
 
     new_msg = calloc(1, struct_size);
 
-    if (decode_snm_hdr(&new_hdr, buf, &rem, &idx) < 0)
+    rem = idx;
+    idx = 0;
+    if (decode_SNM_hdr(&new_hdr, buf, &rem, &idx) < 0)
     {
         traceEvent(TRACE_ERROR, "Failed to decode header");
         goto test_SNM_MSG_err;
     }
-    if (dec(&new_msg, &new_hdr, buf, &rem, &idx))
+
+    log_SNM_hdr(&new_hdr);
+
+    if (dec(new_msg, &new_hdr, buf, &rem, &idx) < 0)
     {
         traceEvent(TRACE_ERROR, "Error decoding message");
         goto test_SNM_MSG_err;
     }
-    if (memcmp(msg, new_msg, struct_size))
+    if (cmp(msg, new_msg))
     {
         traceEvent(TRACE_ERROR, "Mismatched messages");
         goto test_SNM_MSG_err;
     }
+
+    if (hdr->type == SNM_TYPE_REQ_LIST_MSG)
+    {
+        log_SNM_REQ(new_msg);
+        free_communities(&((n2n_SNM_REQ_t *) new_msg)->comm_ptr);
+    }
+    else if (hdr->type == SNM_TYPE_RSP_LIST_MSG)
+    {
+        log_SNM_INFO(new_msg);
+        free_supernodes(&((n2n_SNM_INFO_t *) new_msg)->sn_ptr);
+        free_communities(&((n2n_SNM_INFO_t *) new_msg)->comm_ptr);
+    }
+
+    free(new_msg);
     return 0;
 
 test_SNM_MSG_err:
@@ -69,33 +207,94 @@ test_SNM_MSG_err:
 
 static void test_REQ_LIST()
 {
-    snm_hdr_t      hdr;
+    snm_hdr_t      hdr = {SNM_TYPE_REQ_LIST_MSG, 0, 3134};
     n2n_SNM_REQ_t  req;
     size_t         size = 0;
+    size_t         list_size = 0;
+    struct comm_info *communities = NULL;
+
+    traceEvent( TRACE_INFO, "---- Testing SNM REQUEST message" );
+
+    SET_N(hdr.flags);
+
+    list_size = generate_random_list((void **) &communities,
+                                     sizeof(struct comm_info),
+                                     (rand_func) rand_comm,
+                                     (add_func) comm_list_add);
+
+    req.comm_num = comm_list_size(communities);
+    alloc_communities(&req.comm_ptr, req.comm_num);
+
+    struct comm_info *ci = communities;
+
+    int i;
+    for (i = 0; i < req.comm_num; i++)
+    {
+        req.comm_ptr[i].size = strlen((char *) ci->community_name);
+        memcpy(&req.comm_ptr[i].name, ci->community_name, sizeof(n2n_community_t));
+        ci = ci->next;
+    }
+
+    log_SNM_hdr(&hdr);
+    log_SNM_REQ(&req);
 
     if (test_SNM_MSG(sizeof(n2n_SNM_REQ_t),
                      &hdr, &req, size,
                      (enc_func) encode_SNM_REQ,
-                     (dec_func) decode_SNM_REQ))
+                     (dec_func) decode_SNM_REQ,
+                     cmp_SNM_REQ))
     {
-        traceEvent(TRACE_ERROR, "Error testing n2n_REQ_LIST_t");
+        traceEvent(TRACE_ERROR, "Error testing n2n_SNM_REQ_t");
     }
+
+    free_communities(&req.comm_ptr);
+    clear_comm_list(&communities);
+
+    traceEvent( TRACE_INFO, "---- End testing SNM REQUEST message" );
 }
-static void test_RSP_LIST()
+
+static void test_INFO()
 {
-    snm_hdr_t      hdr;
-    n2n_SNM_RSP_t  rsp;
+    snm_hdr_t      hdr = {SNM_TYPE_REQ_LIST_MSG, 0, 3134};
+    n2n_SNM_REQ_t  req;
+    n2n_SNM_INFO_t rsp;
     size_t         size = 0;
 
-    if (test_SNM_MSG(sizeof(n2n_SNM_RSP_t),
+    traceEvent( TRACE_INFO, "---- Testing SNM INFO message" );
+
+    SET_S(hdr.flags);
+    SET_C(hdr.flags);
+
+    sn_list_t supernodes = { NULL, 0 };
+    generate_random_list((void **) &supernodes.list_head, sizeof(struct sn_info),
+                         (rand_func) rand_sn, (add_func) sn_list_add);
+
+    comm_list_t communities = { NULL, 0 };
+    generate_random_list((void **) &communities.list_head, sizeof(struct comm_info),
+                         (rand_func) rand_comm, (add_func) comm_list_add);
+
+    build_snm_info(&supernodes, &communities, &hdr, &req, &rsp);
+    clear_sn_list(&supernodes.list_head);
+    clear_comm_list(&communities.list_head);
+    hdr.type = SNM_TYPE_RSP_LIST_MSG;
+
+    log_SNM_hdr(&hdr);
+    log_SNM_INFO(&rsp);
+
+    if (test_SNM_MSG(sizeof(n2n_SNM_INFO_t),
                      &hdr, &rsp, size,
-                     (enc_func) encode_SNM_RSP,
-                     (dec_func) decode_SNM_RSP))
+                     (enc_func) encode_SNM_INFO,
+                     (dec_func) decode_SNM_INFO,
+                     cmp_SNM_INFO))
     {
-        traceEvent(TRACE_ERROR, "Error testing n2n_RSP_LIST_t");
+        traceEvent(TRACE_ERROR, "Error testing n2n_SNM_INFO_t");
     }
+
+    clear_snm_info(&rsp);
+
+    traceEvent( TRACE_INFO, "---- End testing SNM INFO message" );
 }
-static void test_ADVERTISE_ME()
+/*static void test_ADVERTISE_ME()
 {
     snm_hdr_t          hdr;
     n2n_SNM_ADV_ME_t   adv;
@@ -108,7 +307,7 @@ static void test_ADVERTISE_ME()
     {
         traceEvent(TRACE_ERROR, "Error testing n2n_ADVERTISE_ME_t");
     }
-}
+}*/
 
 /*
  * LISTS TESTS
@@ -126,21 +325,24 @@ static int test_write_read_list(void       *list,
                                 clear_list_func  cl)
 {
     const char *filename = "tmp_list";
-    void **new_list = NULL;
+    void *new_list = NULL;
 
     if (wr(filename, list))
     {
         traceEvent(TRACE_ERROR, "Error writing list");
         goto out_err;
     }
-    if (rd(filename, new_list))
+    if (rd(filename, &new_list))
     {
         traceEvent(TRACE_ERROR, "Error reading list");
         goto out_err;
     }
-    if (sz(list) != sz(new_list))
+
+    size_t size = sz(list);
+    size_t new_size = sz(new_list);
+    if (size != new_size)
     {
-        traceEvent(TRACE_ERROR, "Mismatched list sizes");
+        traceEvent(TRACE_ERROR, "Mismatched list sizes: %d instead of %d", new_size, size);
         goto out_err;
     }
 
@@ -149,30 +351,34 @@ static int test_write_read_list(void       *list,
 
     while (item && new_item)
     {
-        if (memcmp(item + 1, new_item + 1, item_size))
+#define PTR_SIZE           sizeof(void *)
+#define ITEM_DATA(item)    (void *) ((unsigned int) (item) + PTR_SIZE)
+
+        if (memcmp(ITEM_DATA(item), ITEM_DATA(new_item), item_size - PTR_SIZE))
         {
             traceEvent(TRACE_ERROR, "Mismatched item %d", i);
             goto out_err;
         }
-        item = (void *) *((int *) item);
-        new_item = (void *) *((int *) new_item);
+
+#define NEXT_ITEM(item)    ((void *) *((unsigned int *) item))
+
+        item     = NEXT_ITEM(item);
+        new_item = NEXT_ITEM(new_item);
         i++;
     }
-    if (cl(new_list))
+    if (cl(&new_list) != size)
     {
         traceEvent(TRACE_ERROR, "Error clearing list");
         goto out_err;
     }
 
+    unlink(filename);
     return 0;
 
 out_err:
     unlink(filename);
     return -1;
 }
-
-typedef void (*rand_func) (void *item);
-typedef void (*add_func) (void **list, void *new);
 
 static void test_list(size_t           item_size,
                       rand_func        rand_item,
@@ -182,18 +388,10 @@ static void test_list(size_t           item_size,
                       size_list_func   sz,
                       clear_list_func  cl)
 {
-    int i = 0, size = random() % 100;
-    fprintf(stdout, "Generating %d list items", size);
-
     void *list = NULL;
+    size_t list_size = generate_random_list(&list, item_size, rand_item, add);
 
-    for (i = 0; i < size; i++)
-    {
-        void *new = calloc(1, item_size);
-        rand_item(new);
-        add(&list, new);
-    }
-    if (sz(list) != size)
+    if (sz(list) != list_size)
     {
         traceEvent(TRACE_ERROR, "Mismatched list size");
         return;
@@ -202,23 +400,15 @@ static void test_list(size_t           item_size,
     {
         traceEvent(TRACE_ERROR, "Error write/read test");
     }
-    if (cl(list))
+    if (cl(&list) != list_size)
     {
         traceEvent(TRACE_ERROR, "Error clearing list");
     }
 }
 
-static void rand_sn(struct sn_info *si)
-{
-    si->next = NULL;
-    si->sn.family = AF_INET;
-    unsigned int ipv4 = random() % (~0);
-    memcpy(si->sn.addr.v4, &ipv4, IPV4_SIZE);
-    si->sn.port = random() % ((1 << 16) - 1);
-}
-
 static void test_sn()
 {
+    traceEvent(TRACE_DEBUG, "--- Testing supernodes lists IO");
     test_list(sizeof(struct sn_info),
               (rand_func) rand_sn,
               (add_func) sn_list_add,
@@ -226,23 +416,12 @@ static void test_sn()
               (read_list_func) read_sn_list_from_file,
               (size_list_func) sn_list_size,
               (clear_list_func) clear_sn_list);
-}
-
-static void rand_comm(struct comm_info *ci)
-{
-    ci->next = NULL;
-    ci->sn_num = random() * 10;
-
-    int i = 0, name_size = random() % N2N_COMMUNITY_SIZE;
-    for (; i < name_size - 1; i++)
-    {
-        ci->community_name[i] = random() % ('z' - 'a') + 'a';
-    }
-    ci->community_name[i] = 0;
+    traceEvent(TRACE_DEBUG, "--- End testing supernodes lists IO");
 }
 
 static void test_comm()
 {
+    traceEvent(TRACE_DEBUG, "--- Testing community lists IO");
     test_list(sizeof(struct comm_info),
               (rand_func) rand_comm,
               (add_func) comm_list_add,
@@ -250,14 +429,15 @@ static void test_comm()
               (read_list_func) read_comm_list_from_file,
               (size_list_func) comm_list_size,
               (clear_list_func) clear_comm_list);
+    traceEvent(TRACE_DEBUG, "--- End testing community lists IO");
 }
 
 int main()
 {
     srandom(time(NULL));
     test_REQ_LIST();
-    test_RSP_LIST();
-    test_ADVERTISE_ME();
+    test_INFO();
+    /*test_ADVERTISE_ME();*/
     test_sn();
     test_comm();
     return 0;
