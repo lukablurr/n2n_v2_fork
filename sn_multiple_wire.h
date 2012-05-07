155351aa72d3cc52622ade80d27fa3a4b12cc932
wolf <lukablurr@yahoo.com>
2012-05-07 03:34:39 +0300

    work in progress

--- sn_multiple_wire.h
+++ sn_multiple_wire.h
@@ -14,8 +14,10 @@
 #define SNM_TYPE_REQ_LIST_MSG             0x01
 #define SNM_TYPE_RSP_LIST_MSG             0x02
 #define SNM_TYPE_ADV_MSG                  0x03
+/*
 #define SNM_TYPE_REQ_SUPER_LIST_MSG       0x04
 #define SNM_TYPE_RSP_SUPER_LIST_MSG       0x04
+*/
 
 typedef struct snm_hdr
 {
