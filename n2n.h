bae63dcd88b2619f914f6bc3bce35b2aed96a117
wolf <lukablurr@yahoo.com>
2012-06-03 23:30:16 +0300

    removed N2N_MULTIPLE_SUPERNODES define

--- n2n.h
+++ n2n.h
@@ -40,8 +40,6 @@
 #define _DARWIN_
 #endif
 
-#define N2N_MULTIPLE_SUPERNODES 1
-
 /* Some capability defaults which can be reset for particular platforms. */
 #define N2N_HAVE_DAEMON 1
 #define N2N_HAVE_SETUID 1
